fe56259636f5305f52782908703479f40cb0f097
Minho Son <42821865+smh0505@users.noreply.github.com>
2023-02-13 09:32:20 -0500

    Board feature in progress

--- Script/Board.cs
+++ Script/Board.cs
@@ -22,8 +22,6 @@ namespace GoldenKeyMK3.Script
             _chroma = LoadTexture("Resource/board_chroma.png");
 
             _topicPool = SaveLoad.LoadTopics("board.yml");
-            
-
             GenerateMap();
         }
 
@@ -35,7 +33,51 @@ namespace GoldenKeyMK3.Script
 
         private void GenerateMap()
         {
+            while (_baseTopics.Count < 14)
+            {
+                var headIdx = Rnd.Next(_topicPool.Keys.Count);
+                var topicHead = _topicPool.Keys.ToArray()[headIdx];
+
+                var tailIdx = Rnd.Next(_topicPool[topicHead].Length);
+                var topicTail = _topicPool[topicHead][tailIdx];
+
+                var topic = string.Empty;
+                if (topicHead != "기타") topic += topicHead;
+                topic += " - " + topicTail;
+                _baseTopics.Add(topic);
+            }
+
+            Finish();
+        }
+
+        private void Modify()
+        {
             
+        }
+
+        private void Finish()
+        {
+            var temp = _baseTopics.OrderBy(_ => Rnd.Next()).ToList();
+            for (var i = 0; i < _topics.Length; i++)
+            {
+                switch (i)
+                {
+                    case 6:
+                        _topics[i] = "디맥섬";
+                        break;
+                    case 18:
+                        _topics[i] = "투온섬";
+                        break;
+                    default:
+                        if (_goldenKeys.Contains(i)) _topics[i] = "황금열쇠";
+                        else
+                        {
+                            _topics[i] = temp[0];
+                            temp.RemoveAt(0);
+                        }
+                        break;
+                }
+            }
         }
     }
 }
